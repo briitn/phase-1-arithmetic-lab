9a674e7fdcda44c73225ce9717ff936a37f1382e
princegobah <babynamek@gmail.com>
2021-12-13 23:22:13 -0500

    commit tree 36d1aa670aff82179e8b8199d4b2906d450bdecc

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=2
+const num2=31
+const multiply=(num1*num2)
+const gh=Math.random()*10
+const random=Number.parseInt(gh,10)
 
-// Write your code here
+console.log(random)
+const num3=10
+const num4=6
+const mod=(num3%num4)
+const max=Math.max(20,3,4,5)
